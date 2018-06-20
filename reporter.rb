15266967efdde157de2dc8779b248f93806d0ea7
sandis.babauskis <v-edputk@microsoft.com>
2018-06-20 18:14:12 +0300

    Added reporter.rb for webhook functionality

---
+++ reporter.rb
@@ -0,0 +1,26 @@
+require_relative 'features/support/api_helper'
+require 'json'
+
+thumbnail = {'url' => 'https://i2-prod.mirror.co.uk/incoming/article6193323.ece/ALTERNATES/s615b/Amateur-gardener-Phillip-Vowles-hopes-for-success-with-massive-marrow-which-already-weighs-150Ibs.jpg'}
+
+fields = []
+
+fields.push({'name' => 'Autors', 'value' => 'Sandis'})
+fields.push({'name' => 'Mērķis', 'value' => 'REST API'})
+
+embed = []
+
+embed.push('title' => 'Cucumber',
+           'color' => 2446666,
+           'thumbnail' => thumbnail,
+           'fields' => fields)
+
+payload = {
+    'content' => 'Sandis Babauskis',
+    'embeds' => embed,
+}.to_json
+
+API.post('https://discordapp.com/api/webhooks/459005793627406359/-6FGK6la-XlTO_UZ2G62Cx-TZ0AwmMqIPd3rBkusV1nwPQo1RlmhAp3Kaxn_gHS85Aqs',
+        headers: { 'Content-Type' => 'application/json' },
+        cookies: {},
+        payload: payload)
